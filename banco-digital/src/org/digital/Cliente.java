f31c5a80122253bdda71f6a6f32a98df776b45f0
cdlldanilo <danilocardilli@gmail.com>
2022-11-02 18:26:04 -0300

    Update of the repository of projects

---
+++ banco-digital/src/org/digital/Cliente.java
@@ -0,0 +1,24 @@
+package org.digital;
+
+public class Cliente {
+	
+	private String nome;
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
---
+++ desafio-dio-poo/desafio-dio-poo/README.md
@@ -0,0 +1,3 @@
+# Exemple of Abstraindo um Bootcamp Usando Orientação a Objetos em Java.
+
+## Exemple done in [DIO](https://web.dio.me/ "DIO"). On Bootcamp do ifood.
